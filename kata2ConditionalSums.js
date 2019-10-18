7b30899081a93c0095c499580583ce71310714fb
johnarvi <54685307+johnarvi@users.noreply.github.com>
2019-10-18 18:09:30 -0400

    removed comments

--- kata2ConditionalSums.js
+++ kata2ConditionalSums.js
@@ -1,39 +1,3 @@
-// const conditionalSum = function(values, condition) {
-//   let sum = 0;
-//   if (condition === 'even') {
-//     for (let i = 0; i < values.length; i++) {
-//       if (values[i] % 2 === 0) {
-//         sum += values[i]
-//       }
-//     } return sum;
-//   } else {
-//     for (let i = 0; i < values.length; i++) {
-//       if (values[i] % 2 !== 0) {
-//         sum += values[i]
-//       }
-//     } return sum;
-//   }
-// };
-
-// const conditionalSum = function(values, condition) {
-//   sum = 0;
-//   if (condition === 'even') {
-//     for (let i = 0; i < values.length; i++) {
-//       if (values[i] % 2 === 0) {
-//         sum += values[i]
-//       }         
-//     } return sum;
-//   } else if (condition === 'odd') {
-//     for (let i = 0; i < values.length; i++) {
-//       if (values[i] % 2 !== 0) {
-//         sum += values[i]
-//       }         
-//     } return sum;
-//   } else {
-//     return sum;
-//   }
-// };
-
 const isEven = (num) => num % 2 === 0;
 
 const conditionalSum = function(values, condition) {
@@ -44,8 +8,3 @@ const conditionalSum = function(values, condition) {
   }
   return sum;
 };
-
-// console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
-// console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
-// console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
-// console.log(conditionalSum([], "odd"));
